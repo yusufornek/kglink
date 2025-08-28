01e2186c9a3fc3295f1fcffb8febfdea8fb49187
Yusuf <yusufornek1225@gmail.com>
2025-08-28 15:57:05 +0300

    🔗 Header güncellendi: İletişim → Bağlantılar

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export const Header = async () => {
   const navbarItems = [
-    { href: "/", _title: "İletişim" },
+    { href: "/", _title: "Bağlantılar" },
     { href: "/manifesto", _title: "Manifesto" },
   ]
 
